e6659826e406b8bce6c74d44737d5101b92f2194
Tolsen Wilmot <tolsen2484@gmail.com>
2016-04-06 19:34:32 -0700

    Add puppy class and instances

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+#SPECIES ------------------------
+#Puppy
+
+#CHARACTERISTICS ----------------
+#Eyes: Doe-type 
+#Friendly: extremely
+#Legs: 4
+#Personality: Playful
+#Name: varies
+#Color: varies
+
+#BEHAVIOR -----------------------
+#Rolls
+#Jumps
+#Non-Stop Licking
+#Chews on everything
